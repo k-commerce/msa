1b6a87fa727ac6ef55d0b12d7c2073cb7c2d39f0
kwondongwook <kwondongwook1@gmail.com>
2023-04-18 20:05:33 +0900

    MOVE : QueryDslConfig (global.config -> domain.item.repository)

--- server-item/src/main/java/com/kcommerce/domain/item/repository/QueryDslConfig.java
+++ server-item/src/main/java/com/kcommerce/domain/item/repository/QueryDslConfig.java
@@ -1,4 +1,4 @@
-package com.kcommerce.global.config;
+package com.kcommerce.domain.item.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
